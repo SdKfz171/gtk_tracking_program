66a0f6f486c2dfccd83378e21fe509c35b5fa2f0
DongYoung-Kim <521ab@naver.com>
2022-01-03 21:02:26 +0900

    window show

--- main.c
+++ main.c
@@ -6,9 +6,21 @@
 #include <sqlite3.h>
 #include <curl/curl.h>
 
+// GtkWidget
+GtkBuilder * builder;
+GtkWidget * window;
+
 int main(int argc, char *argv[])
 {
-    puts("include test");
-    
+    gtk_init(&argc, &argv);
+    builder = gtk_builder_new_from_file("ui.glade");
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
+
+    gtk_builder_connect_signals(builder, NULL);
+    g_object_unref(builder);
+
+    gtk_widget_show(window);       
+    gtk_main();
+
     return 0;
 }
