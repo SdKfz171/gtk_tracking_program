4ba7ccc10cfb832f70740894f3f93d7ac352b95f
DongYoung-Kim <521ab@naver.com>
2022-01-03 21:12:47 +0900

    connect signal

--- main.c
+++ main.c
@@ -10,6 +10,61 @@
 GtkBuilder * builder;
 GtkWidget * window;
 
+void on_search_button_clicked(GtkButton * self)
+{
+    puts("search button clicked");
+}
+
+void carrier_combo_changed(GtkComboBoxText * self)
+{
+    puts("carrier combobox changed");
+}
+
+void delete_delete_button_clicked(GtkWidget * self)
+{
+    puts("delete delete button");
+}
+
+void delete_close_button_clicked(GtkWidget * self)
+{
+    puts("delete close button");
+}
+
+void del_prev_track_menu_activate(GtkWidget * self)
+{
+    puts("del menu");
+}
+
+void add_add_button_clicked(GtkWidget * self)
+{
+    puts("add add button");
+}
+
+void add_close_button_clicked(GtkWidget * self)
+{
+    puts("add close button");
+}
+
+void add_prev_track_menu_activate(GtkWidget * self)
+{
+    puts("add menu");
+}
+
+void track_track_button_clicked(GtkWidget * self)
+{
+    puts("track track button");
+}
+
+void track_close_button_clicked(GtkWidget * self)
+{
+    puts("track close button");
+}
+
+void track_track_menu_activate(GtkWidget * self)
+{
+    puts("track menu");
+}
+
 int main(int argc, char *argv[])
 {
     gtk_init(&argc, &argv);
@@ -23,4 +78,9 @@ int main(int argc, char *argv[])
     gtk_main();
 
     return 0;
+}
+
+void on_destroy (GtkWidget * self)
+{
+    gtk_main_quit();
 }
