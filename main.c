36b48365b07649ad4884abae315a28b2e69bbed2
DongYoung-Kim <521ab@naver.com>
2022-01-06 21:10:57 +0900

    .

--- main.c
+++ main.c
@@ -96,6 +96,7 @@ void track_track_menu_activate(GtkWidget * self)
 int main(int argc, char *argv[])
 {
     gtk_init(&argc, &argv);
+    
     builder = gtk_builder_new_from_file("ui.glade");
     window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
 
