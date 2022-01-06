c55ec77eb988788f6ace7735bfa27bf7b43aaee9
DongYoung-Kim <521ab@naver.com>
2022-01-06 21:01:11 +0900

    print list

--- element.c
+++ element.c
@@ -21,6 +21,7 @@ int main(int argc, char *argv[]){
         "array"
     };
 
+    // insert
     for(int i=0; i<10; i++){
         Carrier * carrier = (Carrier *)malloc(sizeof(Carrier));
         sprintf(carrier->id, "%d", i);
@@ -29,6 +30,25 @@ int main(int argc, char *argv[]){
 
         if(!list_size(&list)) list_insert_next(&list, NULL, carrier);
         else list_insert_next(&list, list_tail(&list), carrier);
+    }
+
+    // print
+    ListElmt * element;
+    Carrier * carrier;
+
+    element = list_head(&list);
+    
+    while (1)
+    {
+        carrier = list_data(element);
+        
+        printf("%s: %s, %s\r\n", carrier->id, carrier->name, carrier->tel);
+        // puts("");
+
+        if(list_is_tail(element))
+            break;
+        else
+            element = list_next(element);
     }
 
     return 0;
